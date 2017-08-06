3f33f2b5c9cdda0684971159760b4f0d6ab42601
Darvell <contact@darvell.me>
2017-08-05 17:16:26 -0700

    timesince component

--- src/components/DrugCard.js
+++ src/components/DrugCard.js
@@ -1,135 +1,131 @@
 import {
-    Body,
-    Button,
-    Card,
-    CardItem,
-    Container,
-    Content,
-    Form,
-    Header,
-    Icon,
-    Item,
-    Left,
-    Right,
-    Text
+	Body,
+	Button,
+	Card,
+	CardItem,
+	Container,
+	Content,
+	Form,
+	Header,
+	Icon,
+	Item,
+	Left,
+	Right,
+	Text
 } from 'native-base';
 import { Image, StyleSheet, TouchableHighlight, View } from 'react-native';
 import React, { Component } from 'react';
-
+import TimeSince from 'TimeSince';
 import FlipCard from 'react-native-flip-card';
 import PropTypes from 'prop-types';
 
-const moment = require('moment');
-
 export default class DrugCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            flip: false
-        };
-    }
-    render() {
-        return (
-            <TouchableHighlight onPress={this.props.onPress}>
-                <View>
-                    <FlipCard
-                        flipHorizontal={true}
-                        flipVertical={false}
-                        style={{ flex: 1, borderWidth: 0 }}
-                        clickable={false}
-                        flip={this.state.flip}
-                        friction={15}
-                    >
-                        <Card>
-                            <CardItem>
-                                <Body>
-                                    <Text>
-                                        {this.props.name} ({this.props.measurement})
-                                    </Text>
-                                    <Text note>
-                                        {this.props.timestamp ? (
-                                            'Last taken ' + moment(this.props.timestamp).fromNow()
-                                        ) : (
-                                            'Never taken!'
-                                        )}{' '}
-                                    </Text>
-                                </Body>
-                                <Right>
-                                    {this.props.statistics ? (
-                                        <Button primary transparent onPress={() => this.setState({ flip: true })}>
-                                            <Icon name="ios-information-circle-outline" />
-                                        </Button>
-                                    ) : null}
-                                </Right>
-                            </CardItem>
-                            {this.props.photoUri ? (
-                                <CardItem cardBody>
-                                    <Image
-                                        source={{
-                                            uri: this.props.photoUri
-                                        }}
-                                        style={{ height: 200, width: null, flex: 1 }}
-                                    />
-                                </CardItem>
-                            ) : null}
+	constructor(props) {
+		super(props);
+		this.state = {
+			flip: false
+		};
+	}
+	render() {
+		return (
+			<TouchableHighlight onPress={this.props.onPress}>
+				<View>
+					<FlipCard
+						flipHorizontal={true}
+						flipVertical={false}
+						style={{ flex: 1, borderWidth: 0 }}
+						clickable={false}
+						flip={this.state.flip}
+						friction={15}
+					>
+						<Card>
+							<CardItem>
+								<Body>
+									<Text>
+										{this.props.name} ({this.props.measurement})
+									</Text>
+									<Text note>
+										{this.props.timestamp
+											? <TimeSince timestamp={this.props.timestamp} />
+											: 'Never taken!'}
+									</Text>
+								</Body>
+								<Right>
+									{this.props.statistics
+										? <Button primary transparent onPress={() => this.setState({ flip: true })}>
+												<Icon name="ios-information-circle-outline" />
+											</Button>
+										: null}
+								</Right>
+							</CardItem>
+							{this.props.photoUri
+								? <CardItem cardBody>
+										<Image
+											source={{
+												uri: this.props.photoUri
+											}}
+											style={{ height: 200, width: null, flex: 1 }}
+										/>
+									</CardItem>
+								: null}
 
-                            {this.props.onPressDelete || this.props.onPressEdit ? (
-                                <CardItem style={{ flex: 1, height: 45 }}>
-                                    <Left>
-                                        <Button transparent onPress={this.props.onPressEdit}>
-                                            <Icon name="ios-create-outline" />
-                                        </Button>
-                                    </Left>
-                                    <Body />
-                                    <Right>
-                                        {this.props.onPressDelete ? (
-                                            <Button transparent danger onPress={this.props.onPressDelete}>
-                                                <Icon name="md-trash" />
-                                            </Button>
-                                        ) : null}
-                                    </Right>
-                                </CardItem>
-                            ) : null}
-                        </Card>
-                        {/* Back */}
-                        <Card>
-                            <CardItem>
-                                <Left>
-                                    <Button primary transparent onPress={() => this.setState({ flip: false })}>
-                                        <Icon name="ios-arrow-back" />
-                                    </Button>
-                                </Left>
-                                <Right>
-                                    <Text header>{this.props.name} statistics</Text>
-                                </Right>
-                            </CardItem>
-                            <CardItem cardBody>
-                                {this.props.statistics ? <this.props.statistics /> : null}
-                                <Body>
-                                    <Image
-                                        source={{
-                                            uri: this.photoUri
-                                        }}
-                                        style={{ height: 200, width: null, flex: 1 }}
-                                    />
-                                </Body>
-                            </CardItem>
-                            <CardItem />
-                        </Card>
-                    </FlipCard>
-                </View>
-            </TouchableHighlight>
-        );
-    }
+							{this.props.onPressDelete || this.props.onPressEdit
+								? <CardItem style={{ flex: 1, height: 45 }}>
+										<Left>
+											<Button transparent onPress={this.props.onPressEdit}>
+												<Icon name="ios-create-outline" />
+											</Button>
+										</Left>
+										<Body />
+										<Right>
+											{this.props.onPressDelete
+												? <Button transparent danger onPress={this.props.onPressDelete}>
+														<Icon name="md-trash" />
+													</Button>
+												: null}
+										</Right>
+									</CardItem>
+								: null}
+						</Card>
+						{/* Back */}
+						<Card>
+							<CardItem>
+								<Left>
+									<Button primary transparent onPress={() => this.setState({ flip: false })}>
+										<Icon name="ios-arrow-back" />
+									</Button>
+								</Left>
+								<Right>
+									<Text header>{this.props.name} statistics</Text>
+								</Right>
+							</CardItem>
+							<CardItem cardBody>
+								{this.props.statistics ? <this.props.statistics /> : null}
+								<Body>
+									<Image
+										source={{
+											uri: this.photoUri
+										}}
+										style={{ height: 200, width: null, flex: 1 }}
+									/>
+								</Body>
+							</CardItem>
+							<CardItem />
+						</Card>
+					</FlipCard>
+				</View>
+			</TouchableHighlight>
+		);
+	}
 }
 
 DrugCard.propTypes = {
-    name: PropTypes.string.isRequired,
-    measurement: PropTypes.string.isRequired,
-    photoUri: PropTypes.string,
-    statistics: PropTypes.element,
-    onPressEdit: PropTypes.func,
-    onPressDelete: PropTypes.func,
-    onPress: PropTypes.func,
-    lastEntryTimestamp: PropTypes.number
+	name: PropTypes.string.isRequired,
+	measurement: PropTypes.string.isRequired,
+	photoUri: PropTypes.string,
+	statistics: PropTypes.element,
+	onPressEdit: PropTypes.func,
+	onPressDelete: PropTypes.func,
+	onPress: PropTypes.func,
+	lastEntryTimestamp: PropTypes.number
 };
---
+++ src/components/TimeSince.js
@@ -0,0 +1,42 @@
+import { Component } from 'react';
+import { Text } from 'native-base';
+import PropTypes from 'prop-types';
+const moment = require('moment');
+
+class TimeSince extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			timeString: moment(props.timestamp).fromNow()
+		};
+	}
+
+	componentDidMount() {
+		this.timerUpdate = setInterval(() => {
+			this.setState({ timeString: moment(this.props.timestamp).fromNow() });
+		}, 1000 * 60);
+	}
+
+	componentWillUnmount() {
+		if (this.timerUpdate) {
+			clearInterval(this.timerUpdate);
+		}
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		if (this.state.timeString == nextState.timeString) {
+			return false;
+		}
+		return true;
+	}
+
+	render() {
+		return <Text note>Last taken {this.state.timeString}</Text>;
+	}
+}
+
+TimeSince.PropTypes = {
+	timestamp: PropTypes.number.isRequired
+};
+
+export default TimeSince;
