6a663235e9330b3d28fcc2eaa50c1b17c9ea488d
Darvell Long <contact@darvell.me>
2017-08-05 18:33:12 -0700

    Fix for timecard not compiling and formatting update

--- src/components/TimeSince.js
+++ src/components/TimeSince.js
@@ -1,42 +1,44 @@
-import { Component } from 'react';
-import { Text } from 'native-base';
+import React, { Component } from 'react';
+
 import PropTypes from 'prop-types';
+import { Text } from 'native-base';
+
 const moment = require('moment');
 
 class TimeSince extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			timeString: moment(props.timestamp).fromNow()
-		};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            timeString: moment(props.timestamp).fromNow()
+        };
+    }
 
-	componentDidMount() {
-		this.timerUpdate = setInterval(() => {
-			this.setState({ timeString: moment(this.props.timestamp).fromNow() });
-		}, 1000 * 60);
-	}
+    componentDidMount() {
+        this.timerUpdate = setInterval(() => {
+            this.setState({ timeString: moment(this.props.timestamp).fromNow() });
+        }, 1000 * 60);
+    }
 
-	componentWillUnmount() {
-		if (this.timerUpdate) {
-			clearInterval(this.timerUpdate);
-		}
-	}
+    componentWillUnmount() {
+        if (this.timerUpdate) {
+            clearInterval(this.timerUpdate);
+        }
+    }
 
-	shouldComponentUpdate(nextProps, nextState) {
-		if (this.state.timeString == nextState.timeString) {
-			return false;
-		}
-		return true;
-	}
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.state.timeString == nextState.timeString) {
+            return false;
+        }
+        return true;
+    }
 
-	render() {
-		return <Text note>Last taken {this.state.timeString}</Text>;
-	}
+    render() {
+        return <Text note>Last taken {this.state.timeString}</Text>;
+    }
 }
 
 TimeSince.PropTypes = {
-	timestamp: PropTypes.number.isRequired
+    timestamp: PropTypes.number.isRequired
 };
 
 export default TimeSince;
